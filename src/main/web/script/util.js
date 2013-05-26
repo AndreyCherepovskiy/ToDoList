121211d8e0be82ce2ca3f68611c317d4f90fe325
Andrey <mr.freeze313@gmail.com>
2013-05-26 17:33:43 +0300

    Step 1

---
+++ src/main/web/script/util.js
@@ -0,0 +1,25 @@
+function isEmpty(string){
+    var result = true;
+    for(var i=0;i<string.length; i++){
+        if(string[i] !== " " && string[i]!== "\n"){
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+getCurrentDate = function(){
+    var d = new Date();
+    var month = addZero(d.getMonth() + 1);
+    var hours = addZero(d.getHours());
+    var minutes = addZero(d.getMinutes());
+    var day = addZero(d.getDate());
+    return day + '.' + month + '.' + d.getFullYear() + " " + hours + ':' + minutes;
+};
+addZero = function(arg){
+    if(arg<10){
+        arg = "0" + arg;
+    }
+    return arg;
+};
